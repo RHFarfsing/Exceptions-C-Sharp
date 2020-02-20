6046fd99644446cd0ff2b22e3ff2cfee6786f649
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-20 09:12:49 -0500

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exceptions {
+    class Program {
+        static void Main(string[] args) {
+            var a = 1;
+            var b = 1;
+            try {
+                var x = "ABC";
+                x = null;
+                var len = x.Length;
+            } catch (NullReferenceException ex) {
+                var bce = new BootcampException("Boot camp exception", ex);
+                throw bce;
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            try {
+                var c = a / (b - b);
+            } catch (DivideByZeroException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
